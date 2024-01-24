ea8c8e4d2f42581a9ccf5843139b9405ab5a8b60
POOJA GOPAN <153507248+PoojaGopan@users.noreply.github.com>
2024-01-24 11:11:07 +0530

    Add files via upload

---
+++ jsonparsing.py
@@ -0,0 +1,16 @@
+#######Json to Python Object.
+
+# import json
+# x = '{"name": "lijo", "age": 23}'
+# y = json.loads(x)
+# print(y['name']) 
+# print(y['age'])  # Output: lijo
+
+#import json
+# Assuming 'data.json' contains '{"name": "John", "age": 30, "city": "New York"}'
+#with open('sample.json', 'r') as file:#create a json file with a sample dictionary
+#python_object = json.load(file)
+# Accessing data
+# print(python_object['name'])  # Output: John
+# print(python_object['age'])   # Output: 30
+# print(python_object['city'])
